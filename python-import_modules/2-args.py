81aced8b850132b82a61654b4daaeb54f7045198
niimatik <niimatik.gg@gmail.com>
2025-09-02 15:00:56 +0200

    add: file

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    av = sys.argv
+    l_av = len(av) - 1
+    if l_av > 1:
+        print(l_av, 'arguments:')
+        for i in range(1, l_av + 1):
+            print('{:d}: {}'.format(i, av[i]))
+    if l_av == 1:
+        print(l_av, 'arguments:')
+        for i in range(1, l_av + 1):
+            print('{:d}: {}'.format(i, av[i]))
+    if l_av == 0:
+        print(l_av, 'arguments.')
