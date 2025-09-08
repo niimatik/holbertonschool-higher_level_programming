92239367b836ac02746e34cbd6c4a9c16715dbbe
niimatik <niimatik.gg@gmail.com>
2025-09-08 15:09:26 +0200

    add: file

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a = valid(tuple_a)
+    tuple_b = valid(tuple_b)
+    return ((tuple_a[0] + tuple_b[0]), (tuple_a[1]) + (tuple_b[1]))
+
+def valid(tuple_=()):
+    if tuple_ < 2:
+        if tuple_ == 1:
+            tuple_ = tuple(tuple_[0], 0)
+        if tuple_ == 0:
+            tuple_ = tuple(0, 0)
+    if tuple_ > 2:
+        tuple_ = tuple(tuple_[0], tuple_[1])
+    return (tuple_)
