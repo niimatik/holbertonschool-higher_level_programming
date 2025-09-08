17956cbf48d55f408e522225a92b8b65d3be93d5
niimatik <niimatik.gg@gmail.com>
2025-09-08 10:48:58 +0200

    fix: file

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,5 +3,4 @@ def new_in_list(my_list, idx, element):
     new_list = my_list
     if idx >= 0 and idx < len(new_list):
         new_list[idx] = element
-        return (new_list)
-    return (my_list)
+    return (new_list)
