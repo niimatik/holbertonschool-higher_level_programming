41ab991195e16503b2228b5fc496b804defec96d
niimatik <niimatik.gg@gmail.com>
2025-09-08 15:15:25 +0200

    fix: file

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -5,11 +5,11 @@ def add_tuple(tuple_a=(), tuple_b=()):
     return ((tuple_a[0] + tuple_b[0]), (tuple_a[1]) + (tuple_b[1]))
 
 def valid(tuple_=()):
-    if tuple_ < 2:
-        if tuple_ == 1:
-            tuple_ = tuple(tuple_[0], 0)
-        if tuple_ == 0:
-            tuple_ = tuple(0, 0)
-    if tuple_ > 2:
-        tuple_ = tuple(tuple_[0], tuple_[1])
+    if len(tuple_) < 2:
+        if len(tuple_) == 1:
+            tuple_ = (tuple_[0], 0)
+        if len(tuple_) == 0:
+            tuple_ = (0, 0)
+    if len(tuple_) > 2:
+        tuple_ = (tuple_[0], tuple_[1])
     return (tuple_)
