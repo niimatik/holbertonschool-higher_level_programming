6eca81758d968cee3669fc9e348a13352419e702
niimatik <niimatik.gg@gmail.com>
2025-09-08 11:24:57 +0200

    fix: file

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ''
+    new_str = ''
     for i in my_string:
         if i != 'C' or i != 'c':
-            new_string += i
-    return (new_string)
+            new_str += i
+    return (new_str)
