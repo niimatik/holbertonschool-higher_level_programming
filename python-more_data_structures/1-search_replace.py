3b7648ab036a253f931fa66102ce2105b0a34806
niimatik <niimatik.gg@gmail.com>
2025-09-09 09:43:08 +0200

    add: file

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list
+    for i in new_list:
+        if i == search:
+            i = replace
+    return (new_list)
