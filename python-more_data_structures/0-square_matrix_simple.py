a605c7614e98d0e048046991ebc4dd03650e7d96
niimatik <niimatik.gg@gmail.com>
2025-09-09 09:38:22 +0200

    add: file

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    if len(matrix) > 0:
+        for elem in matrix[:]:
+            new_matrix.append(list(map(lambda x: x ** 2, elem)))
