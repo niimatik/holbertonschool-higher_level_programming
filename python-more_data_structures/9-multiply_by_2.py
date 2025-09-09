afeac096845ca39064b86766acce8138724ebe8b
niimatik <niimatik.gg@gmail.com>
2025-09-09 11:27:34 +0200

    fix: file

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     new_dictionary = {}
     for key in a_dictionary:
-        new_dictionary[key] = new_dictionary[key] * 2
+        new_dictionary[key] = a_dictionary[key] * 2
     return (new_dictionary)
