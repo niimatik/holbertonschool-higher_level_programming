f2f638857d9b62ae3ad0af0e3a0883f6ae13705a
niimatik <niimatik.gg@gmail.com>
2025-09-09 14:33:10 +0200

    fix: file

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if None in a_dictionary:
+    if a_dictionary is None:
         return (None)
     b_value = max(a_dictionary.values(), default=None)
     for k, v in a_dictionary.items():
