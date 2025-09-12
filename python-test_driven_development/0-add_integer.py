016156e458bced078d0e74fab4b4afdd87e5c10f
niimatik <niimatik.gg@gmail.com>
2025-09-12 13:36:34 +0200

    add: documentation

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python3
+"""
+This module defines a function `add_integer` that adds two integers or floats.
+
+If either argument is a float, it will be converted to an integer by truncation (i.e., without rounding).
+If the inputs are not integers or floats, a TypeError is raised.
+"""
 def add_integer(a, b=98):
+    """
+    Adds two numbers after validating and converting them to integers.
+
+    Parameters:
+        a (int or float): The first number to add.
+        b (int or float, optional): The second number to add. Defaults to 98.
+
+    Returns:
+        int: The sum of a and b, both converted to integers.
+
+    Raises:
+        TypeError: If either a or b is not an integer or float.
+    """
     if type(a) not in (int, float):
         raise TypeError('a must be an integer')
     if type(b) not in (int, float):
@@ -9,6 +28,16 @@ def add_integer(a, b=98):
     return a + b
 
 def convert_to_int(num):
+    """
+    Converts a float to an integer by truncation. 
+    If the input is already an integer, it is returned unchanged.
+
+    Parameters:
+        num (int or float): The number to convert.
+
+    Returns:
+        int: The integer value of num.
+    """
     if type(num) is float:
         num = int(num)
     return (num)
