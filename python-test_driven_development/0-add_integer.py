9ec73aa5d23a85356bd785111c5fab3fbd2bc4cf
niimatik <niimatik.gg@gmail.com>
2025-09-12 13:32:14 +0200

    add: file

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    if type(a) not in (int, float):
+        raise TypeError('a must be an integer')
+    if type(b) not in (int, float):
+        raise TypeError('b must be an integer')
+    a = convert_to_int(a)
+    b = convert_to_int(b)
+    return a + b
+
+def convert_to_int(num):
+    if type(num) is float:
+        num = int(num)
+    return (num)
