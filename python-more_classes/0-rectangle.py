e2303c932f361a077bc6a43dc0750e11ea2d17b4
niimatik <niimatik.gg@gmail.com>
2025-09-15 15:20:25 +0200

    add: file

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""rectangle class
+
+This module defines the 'rectangle' class.
+Currently, the class is empty.
+"""
+
+
+class rectangle:
+    """Represents a rectangle (currently has no attributes or methods)."""
+    pass
+
