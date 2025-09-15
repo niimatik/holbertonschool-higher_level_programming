8b6d03412f0577025d05abb0940d2fd34bb28f02
niimatik <niimatik.gg@gmail.com>
2025-09-15 10:18:14 +0200

    add: file

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines the Square class."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initializes a square with the given size."""
+        self.size = size
