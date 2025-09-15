eee2f3efcf95deb8b97dc84d33fc443cdae06c72
niimatik <niimatik.gg@gmail.com>
2025-09-15 10:28:31 +0200

    add: file

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines the Square class."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """Initializes a square with optional size."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
