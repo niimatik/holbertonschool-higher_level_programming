c7bf701cc13d0509fc41b2bd2c79341b5ef6cc43
niimatik <niimatik.gg@gmail.com>
2025-09-15 10:54:20 +0200

    add: file

---
+++ python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines the Square class."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """Initializes a square with optional size."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.size = size
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return self.__size
+    @size.setter
+    def __init__(self, size):
+        """Set the size of the square with validation."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """ return the current square area."""
+        return self.__size ** 2
