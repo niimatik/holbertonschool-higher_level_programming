c2262e2797759509a75034a1b1050f8259a84f90
niimatik <niimatik.gg@gmail.com>
2025-09-15 10:34:52 +0200

    fix: format

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -13,7 +13,5 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
     def area(self):
-        """ return the current square area
-        
-        """
+        """ return the current square area."""
         return (self.__size ** 2)
