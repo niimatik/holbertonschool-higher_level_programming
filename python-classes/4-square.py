b93d295d437798f183016edc28ddf1e91dcd9b4f
niimatik <niimatik.gg@gmail.com>
2025-09-15 10:56:12 +0200

    fix: format

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -12,10 +12,12 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.size = size
+
     @property
     def size(self):
         """Get the size of the square."""
         return self.__size
+
     @size.setter
     def __init__(self, size):
         """Set the size of the square with validation."""
@@ -24,6 +26,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """ return the current square area."""
         return self.__size ** 2
