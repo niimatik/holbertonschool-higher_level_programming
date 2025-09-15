a19c8dd07f01afad3f2b9234fed0c3368b746705
niimatik <niimatik.gg@gmail.com>
2025-09-15 10:06:08 +0200

    fix: format

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""
+Module: square
+This module defines the 'square' class.
+Currently, the class is empty and serves as a placeholder for future development.
+"""
+
 class square:
+    """Represents a square (currently has no attributes or methods)."""
     pass
