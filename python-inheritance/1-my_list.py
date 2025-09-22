0f7b507122823bf2a5e4c95fd2f94e44c3731fc5
niimatik <niimatik.gg@gmail.com>
2025-09-22 11:04:32 +0200

    fix: file

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,7 +6,7 @@ This module defines a custom class `MyList` that inherits from the built-in `lis
 It includes a method to print the list elements in sorted (ascending) order.
 """
 
-class MyList(list):
+class Mylist(list):
     """
     MyList is a subclass of the built-in list.
     
