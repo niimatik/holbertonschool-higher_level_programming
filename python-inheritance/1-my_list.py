65133028621baa76b91e709dac409f7335e5345f
niimatik <niimatik.gg@gmail.com>
2025-09-22 10:33:47 +0200

    add: file

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module: my_list
+
+This module defines a custom class `MyList` that inherits from the built-in `list` class.
+It includes a method to print the list elements in sorted (ascending) order.
+"""
+
+class MyList(list):
+    """
+    MyList is a subclass of the built-in list.
+    
+    It provides an additional method to print the list in sorted order
+    without modifying the original list.
+    """
+
+    def print_sorted(self):
+        """
+        Prints the elements of the list in ascending sorted order.
+
+        This method does not modify the original list.
+        """
+        if issubclass(MyList, list):
+            print(sorted(self))
