bc454f3b1a2daff2b1191fbae9ada8b699dfcdf0
niimatik <niimatik.gg@gmail.com>
2025-09-22 15:32:54 +0200

    fix

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module: square
+
+This module defines a Square class that inherits from Rectangle.
+It ensures the size is validated and passed as both width and height
+to the parent Rectangle class.
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class that inherits from Rectangle.
+    """
+    def __init__(self, size):
+        """Initializes a Square with validated size."""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """
+        Returns a string representation of the square.
+        """
+        return '[square] ' + str(self.__size) + '/' + str(self.__size)
