dafe4963659903576749adc09391d1f22669576c
niimatik <niimatik.gg@gmail.com>
2025-09-22 15:29:58 +0200

    fix

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -12,8 +12,9 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
-    """Square class that inherits from Rectangle."""
-
+    """
+    Square class that inherits from Rectangle.
+    """
     def __init__(self, size):
         """Initializes a Square with validated size."""
         self.integer_validator("size", size)
