a02588537cc5ec3e7709b17b737e6c29b48e90c4
niimatik <niimatik.gg@gmail.com>
2025-09-22 09:58:25 +0200

    fix: format

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def lookup(obj):
-    """Return the list of available attributes and methodes of an object"""
+    """
+    Return the list of available attributes and methodes of an object.
+    """
+
     return dir(obj)
