f7adc98db7689227f8395898d6c9291a3a914303
niimatik <niimatik.gg@gmail.com>
2025-09-22 14:11:58 +0200

    add: file

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module: base_geometry
+
+This module defines an empty class `BaseGeometry`.
+It is intended to be used as a base class for geometric shapes.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty base class for geometry-related operations.
+    """
+    pass
