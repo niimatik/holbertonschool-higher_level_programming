9b742d5958c7af204b5a27c025582a8e2a4856e2
niimatik <niimatik.gg@gmail.com>
2025-09-29 10:46:12 +0200

    add: file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module for appending text to a file.
+
+Provides a function `append_write` that adds content to the end of a UTF-8 text file.
+"""
+
+
+def append_write(filename="", text=""):
+    """Append text to the end of a file and return the number
+    of characters written."""
+
+    with open(filename, 'a', encoding="utf-8") as f:
+        f.write(text)
