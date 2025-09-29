8fae94dfb205f149de7881b2af238c45b201e744
niimatik <niimatik.gg@gmail.com>
2025-09-29 09:54:50 +0200

    fix

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    """Read a file and print is output"""
-
-
+    """Read a file and print its output."""
     with open(filename, encoding="utf-8") as f:
         for line in f:
             print(line, end='')
