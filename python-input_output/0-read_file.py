2d1d51d39edc4326a36e0ebeb088b7b5ac55d3a5
niimatik <niimatik.gg@gmail.com>
2025-09-29 10:33:02 +0200

    fix

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -14,6 +14,7 @@ Functions:
 
 def read_file(filename=""):
     """Read a file and print its output."""
+
     with open(filename, encoding="utf-8") as f:
         for line in f:
             print(line, end='')
--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -13,5 +13,6 @@ Functions:
 
 def write_file(filename="", text=""):
     """Write text to a file and return the number of characters written."""
+
     with open(filename, 'w', encoding="utf-8") as f:
         return f.write(text)
