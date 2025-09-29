a07d15c9761f0e274ff204e098093a099864aef8
niimatik <niimatik.gg@gmail.com>
2025-09-29 10:09:20 +0200

    fix

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
 def read_file(filename=""):
     """Read a file and print its output."""
     with open(filename, encoding="utf-8") as f:
