332348107254411ca42270e728d7b2d77dcf17a4
niimatik <niimatik.gg@gmail.com>
2025-09-29 10:19:01 +0200

    fix: format

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def write_file(filename="", text=""):
+    """Return the number of char print in the file."""
     with open(filename, 'r+', encoding="utf-8") as f:
         return len(f.write(text))
