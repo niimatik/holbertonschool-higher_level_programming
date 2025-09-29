0ccaacce89742ccf616fa70cf8edad57f3a16eb1
niimatik <niimatik.gg@gmail.com>
2025-09-29 16:51:12 +0200

    add: file

---
+++ python-input_output/9-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module defining a Student class with a method to serialize to JSON-compatible dictionary.
+"""
+
+
+class Student:
+    """Class representing a student with basic personal information."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student instance.
+
+        Args:
+            first_name (str): The student's first name.
+            last_name (str): The student's last name.
+            age (int): The student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    def to_json(self):
+        """Return a dictionary representation of the Student instance.
+
+        Returns:
+            dict: A dictionary of the instance attributes.
+        """
+        return self.__dict__
