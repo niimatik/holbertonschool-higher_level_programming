7ff9a0490132c58fd03d1a05895d4b2fd406af19
niimatik <niimatik.gg@gmail.com>
2025-09-29 11:07:42 +0200

    fix

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -2,16 +2,13 @@
 """
 Module for converting Python objects to JSON strings.
 """
-
 from json import dumps
 
 
 def to_json_string(my_obj):
     """Return the JSON representation of a Python object as a string.
-
     Args:
         my_obj: The Python object to convert.
-
     Returns:
         str: The JSON string representation of the object.
     """
