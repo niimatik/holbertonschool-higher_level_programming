27597b1145c5cbc0b008dfc7c7b6da7c1ea20dfc
niimatik <niimatik.gg@gmail.com>
2025-09-30 10:03:05 +0200

    add: file

---
+++ python-input_output/10-student.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Module defining a Student class with a method to
+serialize to JSON-compatible dictionary.
+"""
+
+
+class Student:
+    """Class representing a student with basic personal information."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student instance.
+
+        Args:
+            first_name (str): The student's first name.
+            last_name (str): The student's last name.
+            age (int): The student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+    Returns a dictionary representation of the instance.
+
+    If 'attrs' is a list of strings, only attributes with names in this list
+    will be included in the returned dictionary. If 'attrs' contains any non-string
+    elements or is not a list, all attributes will be returned instead.
+
+    Parameters:
+        attrs (list[str], optional): List of attribute names to include. Defaults to None.
+
+    Returns:
+        dict: A dictionary containing the selected attributes or all attributes if 
+              'attrs' is invalid or not provided.
+    """
+        class_d = self.__dict__
+        sel_d = dict()
+
+        for attr in attrs:
+            if type(attr) is not str:
+                return class_d
+            
+            if attr in class_d:
+                sel_d[attr] = class_d[attr]
+            return class_d
+        return sel_d
