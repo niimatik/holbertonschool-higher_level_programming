c921df1617df621f5212d49ae539b94902f6d8bd
niimatik <niimatik.gg@gmail.com>
2025-10-08 11:29:54 +0200

    fix

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -23,6 +23,7 @@ it's listening on.
 Author: [Your Name or Initials]
 Date: [Optional Date]
 """
+
 import http.server
 import socketserver
 import json
@@ -30,7 +31,7 @@ import json
 PORT = 8000
 
 
-class first_server(http.server.BaseHTTPRequestHandler):
+class FirstServer(http.server.BaseHTTPRequestHandler):
     """
     A simple HTTP server handler that responds to specific GET requests.
 
@@ -43,15 +44,18 @@ class first_server(http.server.BaseHTTPRequestHandler):
     """
 
     def do_GET(self):
-        """Handle GET requests by returning
-        appropriate content based on the path."""
+        """
+        Handle GET requests by returning appropriate content
+        based on the path.
+        """
         if self.path == "/data":
             datas = {"name": "John", "age": 30, "city": "New York"}
             json_string = json.dumps(datas)
             self.send_response(200)
-            self.send_header("content-type", "application/json")
+            self.send_header("Content-Type", "application/json")
             self.end_headers()
             self.wfile.write(json_string.encode("utf-8"))
+
         elif self.path == "/info":
             infos = {
                 "version": "1.0",
@@ -59,29 +63,34 @@ class first_server(http.server.BaseHTTPRequestHandler):
             }
             json_string = json.dumps(infos)
             self.send_response(200)
-            self.send_header("content-type", "application/json")
+            self.send_header("Content-Type", "application/json")
             self.end_headers()
             self.wfile.write(json_string.encode("utf-8"))
+
         elif self.path == "/status":
             self.send_response(200)
-            self.send_header("content-type", "text/plain")
+            self.send_header("Content-Type", "text/plain")
             self.end_headers()
             self.wfile.write("ok".encode("utf-8"))
+
         elif self.path == "/":
             self.send_response(200)
-            self.send_header("content-type", "text/plain")
+            self.send_header("Content-Type", "text/plain")
             self.end_headers()
             self.wfile.write("Hello, this is a simple API!".encode("utf-8"))
+
         else:
             self.send_response(404)
-            self.send_header("content-type", "text/plain")
+            self.send_header("Content-Type", "text/plain")
             self.end_headers()
             self.wfile.write("Endpoint not found".encode("utf-8"))
 
 
 if __name__ == "__main__":
-    """Starts the TCP server on the specified PORT and
-    listens indefinitely for incoming requests."""
-    with socketserver.TCPServer(("", PORT), first_server) as httpd:
-        print("serving at port", PORT)
+    """
+    Starts the TCP server on the specified PORT and
+    listens indefinitely for incoming requests.
+    """
+    with socketserver.TCPServer(("", PORT), FirstServer) as httpd:
+        print("Serving at port", PORT)
         httpd.serve_forever()
