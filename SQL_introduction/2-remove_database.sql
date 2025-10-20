dc993fdced873651677f74235f4d72cedabc19e5
niimatik <niimatik.gg@gmail.com>
2025-10-20 14:18:26 +0200

    fix

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
---  script that deletes the database hbtn_0c_0 in your MySQL server.
-DELETE IF EXISTS hbtn_0c_0;
+-- script that deletes the database hbtn_0c_0 in your MySQL server.
+IF EXISTS hbtn_0c_0 DELETE;
