1ed00422edc7e552b877a9a898638bb0241face6
niimatik <niimatik.gg@gmail.com>
2025-10-20 15:39:40 +0200

    fix

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -3,7 +3,7 @@ CREATE TABLE second_table (
 	id INT,
 	name VARCHAR(256),
 	score INT
-);
+) IF NOT EXISTS;
 INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
 INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
 INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
