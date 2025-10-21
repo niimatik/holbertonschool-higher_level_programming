5ac93f4750afdb1a5fdafcf0021783c208a600b6
niimatik <niimatik.gg@gmail.com>
2025-10-21 15:11:33 +0200

    fix

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- lists all cities contained in the database hbtn_0d_usa.
 SELECT id, name
 FROM cities
-JOIN states ON cities.name = states.name
+JOIN states ON cities.id = states.id
 ORDER BY cities.id ASC;
