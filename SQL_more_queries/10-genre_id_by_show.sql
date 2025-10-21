cf2c90a27c3dcfd03d5b383b01319830082ae797
niimatik <niimatik.gg@gmail.com>
2025-10-21 15:45:01 +0200

    fix

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -2,4 +2,4 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres
 JOIN tv_shows ON tv_show_genres.tv_show_id = tv_shows.id
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
