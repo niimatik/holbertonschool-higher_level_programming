d82d99ae982ed559113b598a83e2500d56463f54
niimatik <niimatik.gg@gmail.com>
2025-10-21 10:24:12 +0200

    fix

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- a script that creates the MySQL server user user_0d_1.
-INSERT INTO user (name)
-VALUES ('user_0d_1')
+DROP USER IF EXISTS 'user_0d_1';
+CREATE USER IF EXISTS 'user_0d_1'
 GRANT ALL PRIVILEGES TO 'user_0d_1'@'localhost';
 [IDENTIFIED BY 'user_0d_1_pwd']
