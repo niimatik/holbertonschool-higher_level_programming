673dd91fb586acfff6a5f4bccf9d2d18b441c5c2
niimatik <niimatik.gg@gmail.com>
2025-10-23 15:24:53 +0200

    fix

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -15,8 +15,8 @@ if __name__ == '__main__':
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states \
-                 WHERE name LIKE BINARY '{}' \
-                 ORDER BY states.id ASC".format(argv[4],))
+                 WHERE name LIKE BINARY %(name)s \
+                 ORDER BY states.id ASC", {'name': argv[4]})
     rows = cur.fetchall()
 
     for row in rows:
