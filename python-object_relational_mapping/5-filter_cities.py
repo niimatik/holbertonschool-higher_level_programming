a65530eaa1f4f39ceffeeeb05292ed61c7de8f08
niimatik <niimatik.gg@gmail.com>
2025-10-24 10:32:49 +0200

    add: task 5

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,25 @@
+#!/user/bin/python3
+"""
+A script that takes in arguments and displays all values in
+the states table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """Access to the database and get the states from the database."""
+
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name \
+                 FROM cities \
+                 JOIN states ON cities.state_id = states.id \
+                 WHERE name LIKE BINARY %(name)s \
+                 ORDER BY cities.id ASC", {'name': argv[4]})
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
