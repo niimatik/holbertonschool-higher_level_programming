6d73bfa4d820bbde76416dfe428fe7f4e271353f
niimatik <niimatik.gg@gmail.com>
2025-10-24 10:47:59 +0200

    fix

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,23 +1,41 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 """
-A script that takes in arguments and displays all values in
-the states table of hbtn_0e_0_usa where name matches the argument.
+This script  takes in the name of a state
+as an argument and lists all cities of that
+state, using the database `hbtn_0e_4_usa`.
 """
 
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    """Access to the database and get the states from the database."""
+    """
+    Access to the database and get the cities
+    from the database.
+    """
 
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states \
-                 WHERE name LIKE BINARY %(name)s \
-                 ORDER BY states.id ASC", {'name': argv[4]})
-    rows = cur.fetchall()
+    with db.cursor() as cur:
+        cur.execute("""
+            SELECT
+                cities.id, cities.name
+            FROM
+                cities
+            JOIN
+                states
+            ON
+                cities.state_id = states.id
+            WHERE
+                states.name LIKE BINARY %(state_name)s
+            ORDER BY
+                cities.id ASC
+        """, {
+            'state_name': argv[4]
+        })
 
-    for row in rows:
-        print(row)
+        rows = cur.fetchall()
+
+    if rows is not None:
+        print(", ".join([row[1] for row in rows]))
