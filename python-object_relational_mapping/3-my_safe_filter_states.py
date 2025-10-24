d9902a1db2429171360f1e6109838dfe716c14bf
niimatik <niimatik.gg@gmail.com>
2025-10-24 10:56:23 +0200

    fix

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 """
-This script  takes in the name of a state
-as an argument and lists all cities of that
-state, using the database `hbtn_0e_4_usa`.
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
 """
 
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    """
-    Access to the database and get the cities
-    from the database.
-    """
+    """Access to the database and get the states from the database."""
 
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
@@ -20,22 +18,19 @@ if __name__ == '__main__':
     with db.cursor() as cur:
         cur.execute("""
             SELECT
-                cities.id, cities.name
+                *
             FROM
-                cities
-            JOIN
                 states
-            ON
-                cities.state_id = states.id
             WHERE
-                states.name LIKE BINARY %(state_name)s
+                name LIKE BINARY %(name)s
             ORDER BY
-                cities.id ASC
+                states.id ASC
         """, {
-            'state_name': argv[4]
+            'name': argv[4]
         })
 
         rows = cur.fetchall()
 
     if rows is not None:
-        print(", ".join([row[1] for row in rows]))
+        for row in rows:
+            print(row)
