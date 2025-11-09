791ed3ad9e64fda307221af7dbc327fe3505118d
niimatik <niimatik.gg@gmail.com>
2025-11-09 23:37:37 +0100

    add

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 
+/* Prints 3 lines but by using an array of string and a loop */
+
 const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 for (let i = 0; i < languages.length; i++) {
