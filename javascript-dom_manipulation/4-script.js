2aeef3c8d17986e36c803778d004c824f3a84392
niimatik <niimatik.gg@gmail.com>
2025-11-10 16:23:04 +0100

    add

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -1,3 +1,10 @@
 #!usr/bin/node
 
+const element = document.getElementById("add_item");
+const retrivedelement = document.querySelector("ul");
 
+element.addEventListener("click", function () {
+  const item = document.createElement("li");
+  item.textContent = "Item";
+  retrivedelement.appendChild(item);
+});
