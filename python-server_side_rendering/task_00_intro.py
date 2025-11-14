7edfb4e5d88aad5fc1367ef555d50d2d7bbc4c9a
niimatik <niimatik.gg@gmail.com>
2025-11-14 10:32:08 +0100

    add

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -22,16 +22,16 @@ def generate_invitations(template, attendees):
         invite = template
 
         name = attendees.get("name") or "N/A"
-        invite = invite.replace("name", name)
+        invite = invite.replace("{name}", name)
 
         event_title = attendees.get("event_title") or "N/A"
-        invite = invite.replace("event_title", event_title)
+        invite = invite.replace("{event_title}", event_title)
 
         event_date = attendees.get("event_date") or "N/A"
-        invite = invite.replace("event_date", event_date)
+        invite = invite.replace("{event_date}", event_date)
 
         event_location = attendees.get("event_location") or "N/A"
-        invite = invite.replace("event_location", event_location)
+        invite = invite.replace("{event_location}", event_location)
 
         filename = f"output_{index}.txt"
         with open(filename, "w") as file:
